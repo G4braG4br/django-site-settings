5e4e1f6baf78b241420b1280f0f14dc52c667af9
Gabriel Osipov <gabriel.osipov@saltpepper.ru>
2026-06-05 13:39:08 +0300

    Refactor and fix utils file

--- django_site_settings/utils.py
+++ django_site_settings/utils.py
@@ -1,40 +1,45 @@
+from typing import Any, List, Optional
 from django.conf import settings
 from django.core.cache import caches
-from .models import AppSetting
-from .models import SiteAnnouncement
+from .models import AppSetting, SiteAnnouncement
+from django.core.cache.backends.base import BaseCache
 
-CACHE_PREFIX = "django_site_setting:"
-SITE_SETTINGS_CACHE_TIMEOUT = getattr(settings, "SITE_SETTINGS_CACHE_TIMEOUT", 86400 * 7)
-SITE_ANNOUNCEMENT_CACHE_TIMEOUT = getattr(settings, "SITE_ANNOUNCEMENT_CACHE_TIMEOUT", 3600)
+CACHE_PREFIX: str = "django_site_setting:"
+SITE_SETTINGS_CACHE_TIMEOUT: int = getattr(settings, "SITE_SETTINGS_CACHE_TIMEOUT", 86400 * 7)
+SITE_ANNOUNCEMENT_CACHE_TIMEOUT: int = getattr(settings, "SITE_ANNOUNCEMENT_CACHE_TIMEOUT", 3600)
 
-CACHE_ALIAS = getattr(settings, "SITE_SETTINGS_CACHE_ALIAS", "default")
+CACHE_ALIAS: str = getattr(settings, "SITE_SETTINGS_CACHE_ALIAS", "default")
 
 
-def get_cache_backend():
+def get_cache_backend() -> BaseCache:
     return caches[CACHE_ALIAS]
 
 
-def get_setting(key: str, default=None):
-    cache = get_cache_backend()
-    cache_key = f"{CACHE_PREFIX}{key}"
-    cached_val = cache.get(cache_key)
+def get_setting(key: str, default: Any = None) -> Any:
+    cache: BaseCache = get_cache_backend()
+    cache_key: str = f"{CACHE_PREFIX}{key}"
+    cached_val: Optional[Any] = cache.get(cache_key)
 
     if cached_val is not None:
         return cached_val
 
     try:
-        setting = AppSetting.objects.get(key=key)
-        val = setting.get_typed_value()
-        cache.set(cache_key, val, timeout=SITE_SETTINGS_CACHE_TIMEOUT)
-        return val
+        setting: AppSetting = AppSetting.objects.get(key=key)
     except AppSetting.DoesNotExist:
         return default
 
+    try:
+        val: Any = setting.get_typed_value()
+        cache.set(cache_key, val, timeout=SITE_SETTINGS_CACHE_TIMEOUT)
+        return val
+    except (ValueError, TypeError):
+        return default
 
-def get_active_announcements():
-    cache = get_cache_backend()
-    cache_key = SiteAnnouncement.SITE_ANNOUNCEMENT_CACHE_KEY
-    announcements = cache.get(cache_key)
+
+def get_active_announcements() -> List[SiteAnnouncement]:
+    cache: BaseCache = get_cache_backend()
+    cache_key: str = SiteAnnouncement.SITE_ANNOUNCEMENT_CACHE_KEY
+    announcements: Optional[List[SiteAnnouncement]] = cache.get(cache_key)
 
     if announcements is None:
         announcements = list(SiteAnnouncement.objects.active())
